23038d9c113b70de6a57c626fe62013213ad92d5
baolam <lam01662052827@gmail.com>
2026-06-14 15:37:16 +0700

    Finish train model

--- tools/train.py
+++ tools/train.py
@@ -41,3 +41,43 @@ def train(args):
     if os.path.exists(os.path.join(train_config['ckpt_name'])):
         print("Loading checkpoint as found one!")
         model.load_state_dict(torch.load(train_config['ckpt_name'], map_location=device))
+
+    num_epochs = train_config['num_epochs']
+    optimizer = Adam(model.parameters(), lr=train_config['lr'])
+    criterion = torch.nn.MSELoss()
+
+    for epoch_idx in range(num_epochs):
+        losses = []
+
+        for im, _ in tqdm(mnist_loader):
+            optimizer.zero_grad()
+
+            im = im.float().to(device)
+
+            noise = torch.rand_like(im).to(device)
+
+            t = torch.randint(0, diffusion_config['num_timesteps'], (im.shape[0], )).to(device)
+
+            noisy_im = scheduler.add_noise(im, noise, t)
+            noise_pred = model(noisy_im, t)
+
+            loss = criterion(noise_pred, noisy_im)
+            losses.append(loss.item())
+
+            loss.backward()
+            optimizer.step()
+        
+        print('Finished epoch:{} | Loss : {:.4f}'.format(
+            epoch_idx + 1,
+            np.mean(losses),
+        ))
+
+        torch.save(model.state_dict(), os.path.join(train_config['ckpt_name']))
+        print("Done training!")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Arguments for ddpm training")
+    parser.add_argument('--config', dest='config_path', default='configs/default.yaml', type=str)
+
+    args = parser.parse_args()
+    train(args)
